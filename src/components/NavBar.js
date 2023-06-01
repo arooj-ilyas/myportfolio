685c5e162afd52412a779062e510f2fe7e8c0fb6
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2023-06-01 15:35:11 -0400

    for navbar, states made to determine which of the pages the user is on while scrolling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,87 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import instaIcon from "../assets/insta-icon-small.png"
-import githubIcon from "../assets/github-icon-small.png"
-import linkedInIcon from "../assets/linkedin-icon-small.png"
+import { useState, useEffect } from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import instaIcon from "../assets/insta-icon-small.png";
+import githubIcon from "../assets/github-icon-small.png";
+import linkedInIcon from "../assets/linkedin-icon-small.png";
 
 const NavBar = () => {
+  const [activePage, setActivePage] = useState("home"); //state to manage which page you are currently scrolled on
+  const [scrolled, setScrolled] = useState(false); //background changes as user scrolls, using this state to detect if use scrolled
+
+  //create a useEffect to determine if user has scrolled or not, trigger once user has scrolled
+  //need to add an event listener on mount so it knows to fire once a scroll has been detected
+  //must remember to remove event listener once the element gets removed from the DOM
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const updateActivePage = (value) => {
+    setActivePage(value)
+  }
+
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={''} alt="Logo"/>
+          <img src={""} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggle-icon"></span>        
+          <span className="navbar-toggle-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">home</Nav.Link>
-            <Nav.Link href="#skills">skills</Nav.Link>
-            <Nav.Link href="#projects">projects</Nav.Link>
-            <Nav.Link href="#connect">connect</Nav.Link>
+            <Nav.Link
+              href="#home"
+              className={ activePage === "home" ? "current navbar-link" : "navbar-link"}
+              onClick={() => updateActivePage()}
+            >
+              home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={activePage === "skills" ? "current navbar-link" : "navbar-link"}
+              onClick={() => updateActivePage()}
+            >
+              skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={activePage === "project" ? "current navbar-link" : "navbar-link" }
+              onClick={() => updateActivePage()}
+            >
+              projects
+            </Nav.Link>
+            {/* <Nav.Link href="#connect">connect</Nav.Link> */}
           </Nav>
+
           <span className="navbar-text">
+            <button className="connectbutton" onClick={() => console.log("connect")}>
+              <span>let's connect</span>
+            </button>
+
             <div className="social-icons">
-                <a href="https://github.com/arooj-ilyas"><img src={githubIcon} alt="insta-icon" /></a>
-                <a href="https://www.linkedin.com/in/arooj-ilyas/"><img src={linkedInIcon} alt="insta-icon" /></a>
-                <a href="https://www.instagram.com/coder.oo/"><img src={instaIcon} alt="insta-icon" /></a>
+              <a href="https://github.com/arooj-ilyas">
+                <img src={githubIcon} alt="insta-icon" />
+              </a>
+              <a href="https://www.linkedin.com/in/arooj-ilyas/">
+                <img src={linkedInIcon} alt="insta-icon" />
+              </a>
+              <a href="https://www.instagram.com/coder.oo/">
+                <img src={instaIcon} alt="insta-icon" />
+              </a>
             </div>
           </span>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
