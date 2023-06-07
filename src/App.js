3107173f2af32effc40239f92758ee7313788820
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2023-06-07 17:02:39 -0400

    elements added to banner component, image imported for name and some basic styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import NavBar from './components/NavBar';
-import Banner from './components/Banner'
+import NavBar from './components/NavBar.js';
+import Banner from './components/Banner.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 function App() {
   return (
   <div className="App">
-    <NavBar />
     <Banner />
+    <NavBar />
   </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Row, Container, Col} from "react-bootstrap"
+import nameImage from "../assets/name-h1-portfolio.png"
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <section className="banner" id="home">
+        <Container>
+            <Row className="align-items-center">
+                <Col xs={12} md={6} xl={7}>
+                    <span className="tagline">welcome to my portfolio</span>
+                    <h1>{`hi 👋🏽`}</h1>
+                    <a href="#!">
+                        <img src={nameImage} alt="name-badge" />
+                    </a>
+                </Col>
+            </Row>
+        </Container>
+    </section>
   )
 }
 
