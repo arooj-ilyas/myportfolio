49f01a469c4dcff2e2ee0903877e7dc3c1bb5ad0
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2023-06-16 12:52:38 -0400

    assets updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { ArrowRightCircle } from 'react-bootstrap-icons'
 import {Row, Container, Col} from "react-bootstrap"
 import nameImage from "../assets/name-h1-portfolio.png"
+import pixelArooj from "../assets/pixel-arooj.png"
 
 const Banner = () => {
   return (
     <section className="banner" id="home">
         <Container>
-            <Row className="align-items-center">
+            <Row className="align-items-center" xs={12} md={6} xl={7}>
                 <Col xs={12} md={6} xl={7}>
                     <span className="tagline">welcome to my portfolio</span>
                     <h1>{`hi 👋🏽`}
@@ -14,7 +16,10 @@ const Banner = () => {
                     <a href="#!">
                         <img src={nameImage} alt="name-badge" />
                     </a>
-                    <button onClick={()=> console.log('connect')}>Let's connect</button>
+                    <button onClick={()=> console.log('connect')}>Let's connect<ArrowRightCircle size={25}/></button>
+                </Col>
+                <Col xs={12} md={6} xl={7}>
+                    <img src={pixelArooj} alt="AroojPixelCharacter"/>
                 </Col>
             </Row>
         </Container>
