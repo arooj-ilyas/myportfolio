754b961784fb74ecc57a7c87509d5b4af1d8fadd
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2023-07-11 18:11:05 -0400

    projects section started, carousel package imported and images for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,63 @@
-import React from 'react'
-import projectsHeading from "../assets/projects-heading.png"
-import { Container } from 'react-bootstrap'
+import React from "react";
+import projectsHeading from "../assets/projects-heading.png";
+import { Container } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import spoilerAlertGraphic from "../assets/spoiler-alert-graphic.png"
+import rubADubGraphic from "../assets/rub-a-dub-graphic.png"
+import modernBankAppGraphic from "../assets/modern-bank-app-graphic.png"
 
 const Projects = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
     <section id="projects">
-    <Container>
+      <Container>
+        <div className="heading-container">
+          <img src={projectsHeading} alt="ProjectsHeading" />
+        </div>
 
-      <div className="heading-container">
-        <img src={projectsHeading} alt="ProjectsHeading" />
-      </div>
-    
-    </Container>
+        <Carousel responsive={responsive} infinite={true} className="projects-slider">
+          <div className="item">
+            <img src={rubADubGraphic} alt="project-graphic"/>
+            <h3>Rub-a-Dub - Developer Hub</h3>
+            <p>Project brief</p>
+          </div>
+          
+          <div className="item">
+            <img src={spoilerAlertGraphic} alt="project-graphic"/>
+            <h3>SpoilerAlert!</h3>
+            <p>Project brief</p>
+          </div>
+          
+          <div className="item">
+            <img src={modernBankAppGraphic} alt="project-graphic"/>
+            <h3>Modern Bank App</h3>
+            <p>Project brief</p>
+          </div>
+
+        </Carousel>
+      </Container>
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
