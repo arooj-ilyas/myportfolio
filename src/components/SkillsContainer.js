c84b4929c2e625aea5f779a412e4556f293b8d88
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2023-07-11 17:19:16 -0400

    clicking on skills in navbar brings skills heading to top of page

--- src/components/SkillsContainer.js
+++ src/components/SkillsContainer.js
@@ -2,12 +2,15 @@ import React from 'react'
 import skillsHeading from "../assets/skills-heading.png"
 import skillsIconsDesktop from "../assets/skills-icons-desktop.png"
 import purpleSkillsContainer from "../assets/purple-skills-container.png"
+import { Container } from 'react-bootstrap'
 
 
 const SkillsContainer = () => {
 
   return (
     <section id="skills">
+    <Container>
+
       <div className="heading-container">
         <img src={skillsHeading} alt="SkillsHeading" />
       </div>
@@ -15,6 +18,7 @@ const SkillsContainer = () => {
       <img className="skills-icons" src={skillsIconsDesktop} alt="SkillsIcons"/>
       <img className="skills-purple-container" src={purpleSkillsContainer} alt="PurpleContainerSkillsText"/>
 
+    </Container>
     </section>
   )
 }
